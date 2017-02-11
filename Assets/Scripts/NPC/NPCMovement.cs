ef92c7a088e364a6d02903039ff50e851dc10e40
Carabella Kozeni <ic16703@essex.ac.uk>
2017-02-11 15:52:15 +0000

    NPC Base Class Done

--- Assets/Scripts/NPC/NPCMovement.cs
+++ Assets/Scripts/NPC/NPCMovement.cs
@@ -8,15 +8,7 @@ public class NPCMovement : MonoBehaviour {
 	private int i = 0;
 	private bool end = false;
 
-
-
-	//public float directionChangeInterval = 1;
-	//public float maxHeadingChange = 30;
 	Vector3 movement =new Vector3();
-
-	
-	//float heading;
-	//Vector3 targetRotation;
 
 
 	// Use this for initialization
@@ -24,37 +16,34 @@ public class NPCMovement : MonoBehaviour {
 		
 	    targetPosition = points[0];
 
-	    //Set random initial rotation
-	    //heading=Random.Range(0,360);
-	    //transform.eulerAngles = new Vector3 (0, heading, 0);
-	    //StartCoroutine (NewHeading ());
-
-
 	}
-	
+
+
+
 	// Update is called once per frame
+	//Patrols in a predefined area
 	void Update ()
 	{
-		//transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+
 		transform.position=Vector3.MoveTowards(transform.position,targetPosition,speed*Time.deltaTime);
-		//var forward = transform.TransformDirection(Vector3.forward);
-		//controller.SimpleMove(forward * speed);
-		if (i < points.Length && end == false) {
+	
+
+		if ( end == false) {
         
 			if (transform.position == targetPosition) {
 				targetPosition = points [i];
 				i++;
 			}
-			if (i == points.Length )
+			if (i == points.Length  )
 				end = true;
 		}
 		
-		if (i > points.Length  && end == true)
+		if (end == true)
 		{
 		    
 			if (transform.position == targetPosition) {
-				targetPosition = points [i];
-				i--;
+				targetPosition = points [--i];
+
 			}
 			if (i == 0)
 				end = false;
@@ -62,27 +51,10 @@ public class NPCMovement : MonoBehaviour {
 
 	}
 
-	/*/// <summary>
-	/// Repeatedly calculates a new direction to move towards.
-	/// Use this instead of MonoBehaviour.InvokeRepeating so that the interval can be changed at runtime.
-	/// </summary>
-	IEnumerator NewHeading ()
+	public void changeTarget(Vector3 location)
 	{
-		while (true) {
-			NewHeadingRoutine();
-			yield return new WaitForSeconds(directionChangeInterval);
-		}
+		targetPosition = location;	
 	}
 
-	/// <summary>
-	/// Calculates a new direction to move towards.
-	/// </summary>
-	void NewHeadingRoutine ()
-	{
-		var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
-		var ceil  = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
-		heading = Random.Range(floor, ceil);
-		targetRotation = new Vector3(0, heading, 0);
-	}
-*/
+
 }
