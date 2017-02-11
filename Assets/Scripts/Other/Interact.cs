31312d82e6b016a96a128ea9642ed8897bd73bd1
Cypry <cypry97@gmail.com>
2017-02-11 15:10:55 +0000

    Basic Interaction Logic

---
+++ Assets/Scripts/Other/Interact.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact : MonoBehaviour {
+    private bool canInteract = false;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            canInteract = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            canInteract = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (canInteract)
+        {
+            if(Input.GetButton("Interact"))
+            {
+
+            }
+        }
+    }
+}
