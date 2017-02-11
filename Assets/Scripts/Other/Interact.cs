3a24191a828f4ba2688f18a2f239df3abd3cdd0f
Cypry <cypry97@gmail.com>
2017-02-11 15:27:24 +0000

    V2

--- Assets/Scripts/Other/Interact.cs
+++ Assets/Scripts/Other/Interact.cs
@@ -3,31 +3,35 @@ using System.Collections;
 
 public class Interact : MonoBehaviour {
     private bool canInteract = false;
+    private bool isInteracting = false;
+
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
-        {
             canInteract = true;
-        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
-        {
             canInteract = false;
-        }
     }
 
     private void Update()
     {
         if (canInteract)
         {
-            if(Input.GetButton("Interact"))
+            if(!isInteracting && Input.GetButton("Interact"))
             {
-
+                isInteracting = true;
+                BeginInteraction();
             }
         }
+    }
+
+    private void BeginInteraction()
+    {
+        //Add interaction code here
     }
 }
