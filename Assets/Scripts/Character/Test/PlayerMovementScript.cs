514c312b73b477bedd68c759f6927c906a25aed3
Thorn <tt16786@essex.ac.uk>
2017-02-11 14:42:15 +0000

    Move better now

--- Assets/Scripts/Character/Test/PlayerMovementScript.cs
+++ Assets/Scripts/Character/Test/PlayerMovementScript.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class PlayerMovementScript : MonoBehaviour
 {
 
-    public float speed;
+    public float speed = 2f;
     public Rigidbody2D rb2d;
                
     void Start()
     {
-        speed = 1f;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
@@ -24,9 +23,9 @@ public class PlayerMovementScript : MonoBehaviour
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-	
-		float x = Input.GetAxis ("Horizontal")*speed*Time.fixedDeltaTime;
-		float y = Input.GetAxis ("Vertical")*speed*Time.fixedDeltaTime;
+
+		float x = Input.GetAxisRaw ("Horizontal")*speed*Time.fixedDeltaTime;
+		float y = Input.GetAxisRaw ("Vertical")*speed*Time.fixedDeltaTime;
 
 		transform.position += new Vector3 (x, y, 0f);
 
