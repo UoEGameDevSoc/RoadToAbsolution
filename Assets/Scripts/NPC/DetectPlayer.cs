cd3f9768edcc0e2a9c7707e1068a62217e7b9857
Cypry <cypry97@gmail.com>
2017-02-18 14:31:18 +0000

    Initial script

---
+++ Assets/Scripts/NPC/DetectPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour {
+    //How wide the cone of vision is (in degrees)
+    [SerializeField]
+    private float detectionRadius = 60f;
+
+    //How long the cone of vision is
+    [SerializeField]
+    private float detectionRange;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
