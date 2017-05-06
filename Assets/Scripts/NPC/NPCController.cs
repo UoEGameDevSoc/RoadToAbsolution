94a187a27a5e8ebaf38ccb889e69ff74876fe71c
irinack <irina.carabella@yahoo.com>
2017-05-06 16:14:53 +0100

    Branch69 (#9)
    * NPC Base Class Done
    
    * Main Char
    
    * MainCharSprites+AnimController
    
    * Delete Character.meta
    
    * Delete Environment.meta
    
    * Delete IconsUI.meta
    
    * Delete Imported piskel.png

---
+++ Assets/Scripts/NPC/NPCController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCController : MonoBehaviour {
+
+	private NPCMovement movementScript;
+    public bool patrol;
+	public bool NPC; // if 0, character is enemy of player
+					// if 1, character is normal NPC
+
+
+	// Use this for initialization
+	void Start () {
+		movementScript = GetComponent<NPCMovement> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    Detect();
+	}
+
+	void Detect()
+	{
+		if(patrol)
+		{
+			movementScript.enabled =true;
+
+		}
+
+		if(!patrol)
+			movementScript.enabled =false;
+	}
+
+}
---
+++ Assets/Scripts/NPC/NPCMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCMovement : MonoBehaviour {
+	public float speed;
+    public Vector3[] points;
+    private Vector3 targetPosition;
+	private int i = 0;
+	private bool end = false;
+
+	Vector3 movement =new Vector3();
+
+
+	// Use this for initialization
+	void Awake () {
+		
+	    targetPosition = points[0];
+
+	}
+
+
+
+	// Update is called once per frame
+	//Patrols in a predefined area
+	void Update ()
+	{
+
+		transform.position=Vector3.MoveTowards(transform.position,targetPosition,speed*Time.deltaTime);
+	
+
+		if ( end == false) {
+        
+			if (transform.position == targetPosition) {
+				targetPosition = points [i];
+				i++;
+			}
+			if (i == points.Length  )
+				end = true;
+		}
+		
+		if (end == true)
+		{
+		    
+			if (transform.position == targetPosition) {
+				targetPosition = points [--i];
+
+			}
+			if (i == 0)
+				end = false;
+		}
+
+	}
+
+	public void changeTarget(Vector3 location)
+	{
+		targetPosition = location;	
+	}
+
+
+}
