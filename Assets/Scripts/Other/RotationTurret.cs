9de4fdbf1708ca8c6c1d188148c0bba03d679fb5
Thorn <tt16786@essex.ac.uk>
2017-05-11 20:47:35 +0100

    Some stuff, I don't even care anymore

--- Assets/Scripts/Other/RotationTurret.cs
+++ Assets/Scripts/Other/RotationTurret.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class RotationTurret : MonoBehaviour {
 
-	public float rotateSpeed = 0.5f;
+	public float rotateSpeed;
+	public float rotateAngle;
 
 	private float initRotation;
+	private float currentRotation;
 
 	// Use this for initialization
 	void Start () {
 		initRotation = transform.rotation.z;
+		print (initRotation);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(new Vector3 (0, 0, rotateSpeed*Time.deltaTime));
+		currentRotation = transform.rotation.z;
+		if (initRotation + rotateAngle < currentRotation) {
+			rotateSpeed = -Mathf.Abs (rotateSpeed);
+		} else if (initRotation > currentRotation) {
+			rotateSpeed = Mathf.Abs (rotateSpeed);
+		}
 	}
 }
